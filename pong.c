a0725cadac55ce7d558c3719307fab7215840afd
Stefan Eng <eng_steff@hotmail.com>
2016-06-13 09:19:15 +0200

    Set up buffers and compile shaders.

--- pong.c
+++ pong.c
@@ -35,7 +35,48 @@ void react_to_events(GLFWwindow * window) {
     }
 }
 
+const GLchar * source_vertex_shader = \
+    "#version 330 core\n"
+    "layout (location=0) in vec3 position;\n"
+    "\n"
+    "void main() {"
+    "   gl_Position = vec4(position, 1.0f);\n"
+    "}\n";
+
+const GLchar * source_fragment_shader = \
+    "#version 330 core\n"
+    "out vec4 color;\n"
+    "void main() {\n"
+    "   color = vec4(1.0f, 1.0f, 1.0f, 1.0f);\n"
+    "}\n";
+
+const GLfloat vertices[] = {
+     -0.5f, 0.5f, 1.0f,
+      0.5f, 0.5f, 1.0f,
+      0.0f,-0.5f, 1.0f,
+};
+
+GLint shader_compile(GLuint shader_id, char * buffer_info, size_t size_buffer) {
+
+    /* Compile shader. */
+    glCompileShader(shader_id);
+
+    /* Retrieve context data. */
+    GLint success = 0;
+    glGetShaderiv(shader_id, GL_COMPILE_STATUS, &success);
+
+    /* Get error if there was one. */
+    if (!success) {
+        glGetShaderInfoLog(shader_id, size_buffer, NULL, buffer_info);
+    }
+    return success;
+}
+
 int main(void) {
+
+    // ================================================================
+    // == Window and context setup.
+    // ================================================================
 
     GLFWwindow * window;
 
@@ -68,6 +109,71 @@ int main(void) {
     if(glewInit() != GLEW_OK) {
         error("Could not initialize glew.\n", true);
     }
+
+    // ================================================================
+    // == Buffers.
+    // ================================================================
+
+    /* Create buffers. */
+    GLuint VBO, VAO;
+
+    /* Generate empty vertex and buffer object. */
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+
+    /* Bind vertex array object. */
+    glBindVertexArray(VAO);
+
+    /* Bind buffer object */
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    /* Populate buffer with data. */
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    /* Set vertex attribute pointer for position attribute. */
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3*sizeof(GLfloat), (GLvoid*)0);
+    /* Enable vertex attribute pointer. */
+    glEnableVertexAttribArray(0);
+
+    /* Unbind buffer object. */
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+    // ================================================================
+    // == Shaders.
+    // ================================================================
+
+    /* Create variables for fragment and vertex shader. */
+    GLuint shader_fragment, shader_vertex;
+
+    /* Create storage for shaders. */
+    shader_vertex = glCreateShader(GL_VERTEX_SHADER);
+    shader_fragment = glCreateShader(GL_FRAGMENT_SHADER);
+
+    /* Bind shader sources. */
+    glShaderSource(shader_vertex, 1, &source_vertex_shader, 0);
+    glShaderSource(shader_fragment, 1, &source_fragment_shader, 0);
+
+    /* Compile shaders. */
+    GLint status_shader;
+    size_t size_buffer = 1024;
+    GLchar buffer_error[size_buffer];
+
+    /* Compile vertex shader and print errors. */
+    status_shader = shader_compile(shader_vertex, buffer_error , size_buffer);
+    if (!status_shader) {
+        error("Error in vertex shader:\n", false);
+        error(buffer_error, false);
+    }
+
+    /* Compile fragment shader and print errors. */
+    status_shader = shader_compile(shader_fragment, buffer_error , size_buffer);
+    if (!status_shader) {
+        error("Error in fragment shader:\n", false);
+        error(buffer_error, false);
+    }
+
+    // ================================================================
+    // == Main loop.
+    // ================================================================
 
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
