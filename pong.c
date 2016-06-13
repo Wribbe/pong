0160f8790ad6a4f1874d09b0c8244298baf35f82
Stefan Eng <eng_steff@hotmail.com>
2016-06-13 08:13:21 +0200

    Make window close when ESC is pressed.

--- pong.c
+++ pong.c
@@ -6,11 +6,32 @@
 
 #define UNUSED(x) (void) x
 
+bool map_keys[1024];
+
+void key_callback(GLFWwindow * window, int key, int scancode, int action, int mods) {
+
+    UNUSED(scancode);
+    UNUSED(mods);
+    UNUSED(window);
+
+    if (action == GLFW_PRESS) {
+        map_keys[key] = true;
+    } else if (action == GLFW_RELEASE && map_keys[key]) {
+        map_keys[key] = false;
+    }
+}
+
 void error(const char * message, bool fatal) {
     fprintf(stderr, "ERROR: %s", message);
     if (fatal) {
         fprintf(stderr, "ERROR: Shutting down.\n");
         exit(EXIT_FAILURE);
+    }
+}
+
+void react_to_events(GLFWwindow * window) {
+    if (map_keys[GLFW_KEY_ESCAPE]) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
     }
 }
 
@@ -40,6 +61,8 @@ int main(void) {
         error("Could not create a window.\n", true);
     }
 
+    glfwSetKeyCallback(window, key_callback);
+
     glfwMakeContextCurrent(window);
     glewExperimental = GL_TRUE;
     if(glewInit() != GLEW_OK) {
@@ -48,6 +71,7 @@ int main(void) {
 
     while(!glfwWindowShouldClose(window)) {
         glfwPollEvents();
+        react_to_events(window);
 
         glfwSwapBuffers(window);
     }
