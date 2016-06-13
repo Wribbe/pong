014619e8ed62d8812c166ed55980c72b21fab6f7
Stefan Eng <eng_steff@hotmail.com>
2016-06-13 16:41:06 +0200

    Attach shaders to shader program and link.

--- pong.c
+++ pong.c
@@ -56,7 +56,7 @@ const GLfloat vertices[] = {
       0.0f,-0.5f, 1.0f,
 };
 
-GLint shader_compile(GLuint shader_id, char * buffer_info, size_t size_buffer) {
+GLint shader_compile(GLuint shader_id, char * buffer_info, size_t s_buffer_info) {
 
     /* Compile shader. */
     glCompileShader(shader_id);
@@ -67,7 +67,33 @@ GLint shader_compile(GLuint shader_id, char * buffer_info, size_t size_buffer) {
 
     /* Get error if there was one. */
     if (!success) {
-        glGetShaderInfoLog(shader_id, size_buffer, NULL, buffer_info);
+        glGetShaderInfoLog(shader_id, s_buffer_info, NULL, buffer_info);
+    }
+    return success;
+}
+
+GLint program_link(GLuint shader_program,
+                   GLuint * ids,
+                   size_t num_shaders,
+                   char * buffer_info,
+                   size_t size_buffer) {
+    /* Attach and link the shaders in ids array. Check status and gather any
+     * information on failure and store in buffer_info. Return status. */
+
+    /* Attach all shaders ids. */
+    for(size_t i=0; i<num_shaders; i++) {
+        glAttachShader(shader_program, ids[i]);
+    }
+
+    /* Link all the attached shades. */
+    glLinkProgram(shader_program);
+
+    /* Examine the linking status. */
+    GLint success;
+    glGetProgramiv(shader_program, GL_LINK_STATUS, &success);
+
+    if (!success) {
+        glGetProgramInfoLog(shader_program, size_buffer, NULL, buffer_info);
     }
     return success;
 }
@@ -153,22 +179,40 @@ int main(void) {
     glShaderSource(shader_fragment, 1, &source_fragment_shader, 0);
 
     /* Compile shaders. */
-    GLint status_shader;
-    size_t size_buffer = 1024;
-    GLchar buffer_error[size_buffer];
+    GLint success;
+    size_t s_buffer_info = 1024;
+    GLchar buffer_info[s_buffer_info];
 
     /* Compile vertex shader and print errors. */
-    status_shader = shader_compile(shader_vertex, buffer_error , size_buffer);
-    if (!status_shader) {
+    success = shader_compile(shader_vertex, buffer_info , s_buffer_info);
+    if (!success) {
         error("Error in vertex shader:\n", false);
-        error(buffer_error, false);
+        error(buffer_info, false);
     }
 
     /* Compile fragment shader and print errors. */
-    status_shader = shader_compile(shader_fragment, buffer_error , size_buffer);
-    if (!status_shader) {
+    success = shader_compile(shader_fragment, buffer_info , s_buffer_info);
+    if (!success) {
         error("Error in fragment shader:\n", false);
-        error(buffer_error, false);
+        error(buffer_info, false);
+    }
+
+    /* Set up shader program. */
+    GLuint shader_program = glCreateProgram();
+
+    /* Make shader list */
+    GLuint shaders[] = {shader_vertex, shader_fragment};
+
+    /* Attach shaders to shader program and link. */
+    success = program_link(shader_program,
+                           shaders,
+                           sizeof(shaders),
+                           buffer_info,
+                           s_buffer_info);
+
+    if(!success) {
+        error("Error at program linkage:\n", false);
+        error(buffer_info, false);
     }
 
     // ================================================================
@@ -176,9 +220,16 @@ int main(void) {
     // ================================================================
 
     while(!glfwWindowShouldClose(window)) {
+
+        /* Poll for events. */
         glfwPollEvents();
+
+        /* React to polled events. */
         react_to_events(window);
 
+        /* Render the things. */
+
+        /* Swap buffers. */
         glfwSwapBuffers(window);
     }
 }
