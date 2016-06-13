a8e2cdbdc064375adb5b7e5e5f08746e6ef674e1
Stefan Eng <eng_steff@hotmail.com>
2016-06-13 07:29:56 +0200

    Error function, UNUSED macro.

--- pong.c
+++ pong.c
@@ -1,6 +1,28 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define UNUSED(x) (void) x
+
+void error(const char * message, bool fatal) {
+    printf("ERROR: %s", message);
+    if (fatal) {
+        printf("ERROR: Shutting down.\n");
+        exit(EXIT_FAILURE);
+    }
+}
 
 int main(void) {
-    printf("This is pong!\n");
+
+    GLFWwindow * window;
+
+    UNUSED(window);
+
+//    if(!glfwInit()) {
+
+    error("Non fatal error.\n", false);
+    error("Fatal error.\n", true);
+
 }
