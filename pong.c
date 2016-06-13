a139ec29086e046a1ed22da958ab1b9f6137b404
Stefan Eng <eng_steff@hotmail.com>
2016-06-13 07:55:09 +0200

    Set up window context, create window and loop.

--- pong.c
+++ pong.c
@@ -18,11 +18,37 @@ int main(void) {
 
     GLFWwindow * window;
 
-    UNUSED(window);
+    if(!glfwInit()) {
+        error("Could not initialize GLFW.\n", true);
+    }
 
-//    if(!glfwInit()) {
+    /* Window dimensions. */
+    GLint WIDTH = 800;
+    GLint HEIGHT = 600;
 
-    error("Non fatal error.\n", false);
-    error("Fatal error.\n", true);
+    /* OpenGL window context hints. */
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    const char * title = "PONG.";
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, title, NULL, NULL);
+
+    if(!window) {
+        error("Could not create a window.\n", true);
+    }
+
+    glfwMakeContextCurrent(window);
+    glewExperimental = GL_TRUE;
+    if(glewInit() != GLEW_OK) {
+        error("Could not initialize glew.\n", true);
+    }
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glfwSwapBuffers(window);
+    }
 }
